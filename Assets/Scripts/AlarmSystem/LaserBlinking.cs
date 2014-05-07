7fdb1149aaaa9c16e8d2fd16cae0c27642c344a2
Amin Alizadeh <amin.alizadeh@gmail.com>
2014-05-08 00:51:59 +0300

    P3

---
+++ Assets/Scripts/AlarmSystem/LaserBlinking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBlinking : MonoBehaviour {
+	public float onTime;
+	public float offTime;
+	
+	private float timer;
+	
+	void Update() {
+		if(renderer.enabled && timer >= offTime){
+			SwitchBeam();
+		}
+		
+		if(!renderer.enabled && timer >= offTime){
+			SwitchBeam();
+		}
+	}
+	
+	void SwitchBeam() {
+		timer = 0f;
+		
+		renderer.enabled = !renderer.enabled;
+		light.enabled = !light.enabled;
+	}
+
+}
